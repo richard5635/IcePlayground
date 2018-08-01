d27d806c170f99addc4694efcf5ea015531df3ad
richard5635 <richard.sh093@gmail.com>
2018-08-01 21:51:54 +0900

    first commit

---
+++ Assets/Scripts/UserMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserMovement : MonoBehaviour {
+	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// transform.Rotate(new Vector3(
+		// 	Input.GetAxis("Vertical"),
+		// 	Input.GetAxis("Horizontal"),
+		// 	0
+		// ));
+		transform.eulerAngles += new Vector3(
+			Input.GetAxis("Vertical"),
+			Input.GetAxis("Horizontal"),
+			0
+		);
+	}
+}
